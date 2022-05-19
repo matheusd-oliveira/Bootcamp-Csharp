44061e537d89c84e70f65f6910024b765d0e41b3
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-19 20:10:03 -0300

    criando Demo2
    Foi criada a função de ALTERARNOME, para alterar o nome escolhido pelo usuário que está no Array.
    Na função Main(), temos o array e as variáveis que armazenam o nome a ser substituido e o nome a ser colocado no lugar dele.

--- TrabalhandoComRef/Program.cs
+++ TrabalhandoComRef/Program.cs
@@ -8,15 +8,40 @@ class Ref
         a += 20;
     }
 
+    static void AlterarNome(string[] nomes, string nomeAtual, string nomeNovo)
+    {
+        for (int i = 0; i < nomes.Length; i++)
+        {
+            if (nomes[i] == nomeAtual)
+            {
+                nomes[i] = nomeNovo;
+            }
+        }
+    }
     static void Demo1()
     {
         int a = 5;
         Adicionar20(ref a);
         WriteLine($"O valor de a é {a}!");
     }
+
+
     static void Main()
     {
+        var nomes = new string[] { "Matheus", "Davi", "Narlynho", "Biel", "Felipe" };
 
+        WriteLine($@"A lista de nomes é:
+            {string.Join(", \n", nomes)}
+        ");
 
-    }
+        WriteLine("Digite o nome a ser substituido");
+        var nomeAtual = ReadLine();
+        WriteLine("Digite o nome novo");
+        var nomeNovo = ReadLine();
+
+        AlterarNome(nomes, nomeAtual, nomeNovo);
+        WriteLine($@"A lista de nomes alterada é:
+            {string.Join(", \n", nomes)}
+        ");
+    }   
 }
