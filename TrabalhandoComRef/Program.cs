3eebe535bf9cd428cebb3fcda2d87cf881a0aa65
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-18 23:14:52 -0300

    update

---
+++ TrabalhandoComRef/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+class Ref
+{
+
+    static void Adicionar20(int a)
+    {
+        a += 20;
+    }
+    static void Main()
+    {
+
+        int a = 5;
+        Adicionar20(a);
+        WriteLine($"O valor de a é {a}!");
+    }
+}
