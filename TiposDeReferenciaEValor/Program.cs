bd142b3e7221675cfe92f099a586b26fe7a6efc6
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-19 21:41:43 -0300

    Aprendendo como comparar Referencia e valores
    Nesse projeto é possível ver a diferença entre comparar dois objetos e dois valores que os objetos possuem.

---
+++ TiposDeReferenciaEValor/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+// Comparando referências
+// Logo, as referencias são diferentes. Para serem iguais, é necessário comparar valores.
+
+Numero a = new Numero(2);
+Numero b = new Numero(2);
+
+if (a == b)
+    WriteLine("a e b são iguais");
+else
+    WriteLine("a e b são diferentes");
+
+// Comparando os valores
+if (a.N == b.N)
+    WriteLine("a e b são iguais");
+else
+    WriteLine("a e b são diferentes");
+
+class Numero
+{
+    public int N { get; set; }
+
+    public Numero(int n)
+    {
+        N = n;
+    }
+}
+
+
