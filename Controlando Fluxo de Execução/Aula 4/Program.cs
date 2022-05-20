8699286cd17595e0b673295036cf31295b2e0779
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-19 23:41:06 -0300

    Criando aplicação para entender sobre o break e o continue no C#

---
+++ Controlando Fluxo de Execução/Aula 4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace Aula_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool condicao = true;
+            int valor;
+
+            while (condicao)
+            {
+                WriteLine("Digite um valor. 0 para sair.");
+                valor = int.Parse(ReadLine());
+
+
+                if (valor == 0)
+                {
+                    WriteLine("Você saiu da aplicação");
+                    break;
+                }
+                else
+                {
+                    WriteLine("O valor informado é:" + valor);
+                    WriteLine();
+                    continue;
+
+                }
+            }
+        }
+    }
+}
