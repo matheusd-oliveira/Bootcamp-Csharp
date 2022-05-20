9b9a68fadc0d99a27da578e8b9f4c1dfe9826bd1
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-19 23:21:52 -0300

    Entendendo sobre While/For/Foreach

---
+++ Controlando Fluxo de Execução/Aula 3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using static System.Console;
+namespace Aula_3
+{
+    class Program
+    {
+
+        static void Demo1()
+        {
+            bool condicao = true;
+            int valor;
+
+            while (condicao)
+            {
+                WriteLine("Digite um valor. 0 para sair.");
+                valor = int.Parse(ReadLine());
+
+
+                if (valor == 0)
+                {
+                    WriteLine("Você saiu da aplicação");
+                    condicao = false;
+                }
+                else
+                    WriteLine("O valor informado é:" + valor);
+                WriteLine();
+            }
+        }
+
+        static void Demo2()
+        {
+            int valor;
+            WriteLine("Digite um valor");
+            valor = int.Parse(ReadLine());
+
+            for (int i = valor; i <= 10; i++)
+            {
+                WriteLine(i);
+            }
+        }
+        static void Main()
+        {
+            int[] lista = { 1, 2, 3, 4, 5, };
+
+            foreach(int numero in lista)
+                WriteLine(numero);
+        }
+    }
+}
+
