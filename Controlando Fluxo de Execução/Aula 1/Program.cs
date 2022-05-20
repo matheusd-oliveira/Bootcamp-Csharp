4ca390a55dc6ab7bd265a86439cf8a06037840d3
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-19 22:34:37 -0300

    commit new project

--- Controlando Fluxo de Execução/Aula 1/Program.cs
+++ Controlando Fluxo de Execução/Aula 1/Program.cs
@@ -1,8 +1,27 @@
 ﻿using static System.Console;
 
+class Program
+{
+    static void Main()
+    {
 
-static void Main(){
+        int valor1;
+        int valor2;
+        int total;
 
-    WriteLine("Olá Mundo!");
+        WriteLine("Digite o primeiro valor");
+        valor1 = int.Parse(ReadLine());
+        WriteLine($"O primeiro valor é: {valor1}");
 
+        WriteLine("Digite o segundo valor");
+        valor2 = int.Parse(ReadLine());
+        WriteLine($"O segundo valor é: {valor2}");
+
+        total = valor1 + valor2;
+        WriteLine($"A soma dos dois valores é: {total}");
+
+
+
+
+    }
 }
---
+++ Controlando Fluxo de Execução/Aula 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace Aula_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int idade;
+
+            Write("Digite a sua idade: ");
+            idade = int.Parse(ReadLine());
+
+            if (idade >= 18)
+                WriteLine("Você é maior de idade!");
+            else
+                WriteLine("Você é MENOR de idade!");
+        }
+    }
+}
