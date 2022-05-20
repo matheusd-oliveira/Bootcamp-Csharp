a9a16fe14d0f456b6b9cd9c7ab417f4f1c6542ad
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-19 21:32:41 -0300

    implementando uma interface e mudando de ref struct para struct

--- RefWithStruct/Objetos.cs
+++ RefWithStruct/Objetos.cs
@@ -1,20 +1,32 @@
-public ref struct Pessoa{
+public struct Pessoa
+{
 
     public int Idade { get; set; }
 
     public string Nome { get; set; }
 
-    public Endereco EnderecoPessoa {get; set;}
+    public Endereco EnderecoPessoa { get; set; }
 }
 
 
-public ref struct Endereco {
+public struct Endereco : Dados
+{
 
     public int Numero { get; set; }
 
     public string Logradouro { get; set; }
 
-    public string CEP { get; set;}
+    public string CEP { get; set; }
 
     public string Cidade { get; set; }
+
+
+}
+
+public interface Dados
+{
+
+    int Numero { get; set; }
+
+    string Logradouro { get; set; }
 }
