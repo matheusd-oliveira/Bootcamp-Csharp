f3a5b47f09d961bac20c643366a9c76fc7e3b6a1
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-25 06:41:27 -0300

    percorrendo matriz

--- Fundamentos de Coleções e LINQ com .NET/Arrays/Colecoes/Program.cs
+++ Fundamentos de Coleções e LINQ com .NET/Arrays/Colecoes/Program.cs
@@ -6,19 +6,31 @@ namespace Colecoes
     class Program
     {
         static void Main(string[] args)
-        {   
-            
-            int[,] matriz = new int[4, 2];
-            
+        {
 
-            
+            int[,] matriz = new int[4, 2]{
+                { 8, 8},
+                {10, 20},
+                {5, 100},
+                {90, 200}
+            };
+
+            for (int l = 0; l < matriz.GetLength(0); l++)
+            {
+                for (int c = 0; c < matriz.GetLength(1); c++)
+                {
+                    WriteLine(matriz[l, c]);
+                }
+            }
+
+
             // int[] arrayInteiros = new int[3];
 
             // arrayInteiros[0] = 10;
             // arrayInteiros[1] = 20;
             // arrayInteiros[2] = int.Parse("30");
-            
-            
+
+
             // WriteLine("Percorrendo array pelo for");
             // for (int i = 0; i < arrayInteiros.Length; i++)
             // {
