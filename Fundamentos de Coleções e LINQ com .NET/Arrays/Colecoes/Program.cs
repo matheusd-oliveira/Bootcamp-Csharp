b854880b146372bc29944146d88b6fe369ab59dd
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-25 06:13:35 -0300

    Criando solution e projeto console.

---
+++ Fundamentos de Coleções e LINQ com .NET/Arrays/Colecoes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Colecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
