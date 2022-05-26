ab72f42826f0b8207d47729d70d830bd1079cb09
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-25 23:53:48 -0300

    trabalhando com listas

--- Fundamentos de Coleções e LINQ com .NET/Coleções Genéricas/Listas/Program.cs
+++ Fundamentos de Coleções e LINQ com .NET/Coleções Genéricas/Listas/Program.cs
@@ -1,14 +1,20 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Listas
 {
     class Program
     {
         static void Main(string[] args)
         {
-           List<string> estados = [];
+           List<string> estados = new List<string>();
+           estados.Add("BA");
+           estados.Add("MG");
+           estados.Add("SP");
+           estados.Add("RJ");
+		   System.Console.WriteLine($"Quantidade de elementos da lista: {estados.Count}");
 
-           
+
+        
 
         }
     }
