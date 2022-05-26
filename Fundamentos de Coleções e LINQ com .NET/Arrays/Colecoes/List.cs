5751df97d797fc564b1fba37cbf3871e2e5a1a2b
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-26 00:43:52 -0300

    Adicionando outra coleção a lista.

--- Fundamentos de Coleções e LINQ com .NET/Arrays/Colecoes/List.cs
+++ Fundamentos de Coleções e LINQ com .NET/Arrays/Colecoes/List.cs
@@ -10,6 +10,8 @@ namespace Colecoes
             OperacoesLista opLista = new OperacoesLista();
 
             List<string> estados = new List<string>();
+            string[] estadosArray = new string[2]{"SC", "MT"}; 
+
             estados.Add("BA");
             estados.Add("SP");
             estados.Add("MG");
@@ -17,12 +19,15 @@ namespace Colecoes
             System.Console.WriteLine($"A quantidade de elementos na lista: {estados.Count}");
             
             opLista.ImprimirListaString(estados);
-
-            System.Console.WriteLine("Removendo o elemento");
-            estados.Remove("MG");
-            opLista.ImprimirListaString(estados);
-
             
+            // Adicionando outra coleção dentro da lista.
+            estados.AddRange(estadosArray);
+            opLista.ImprimirListaString(estados);
+            // System.Console.WriteLine("Removendo o elemento");
+            // estados.Remove("MG");
+            // opLista.ImprimirListaString(estados);
+
+
         }
     }
 }
