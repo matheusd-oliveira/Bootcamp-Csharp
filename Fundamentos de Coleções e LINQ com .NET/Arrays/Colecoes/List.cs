3924abbb6a5a97733f03a94fe964159c7d8349d8
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-26 21:48:39 -0300

    aprendendo sobre dicionarios

--- Fundamentos de Coleções e LINQ com .NET/Arrays/Colecoes/List.cs
+++ Fundamentos de Coleções e LINQ com .NET/Arrays/Colecoes/List.cs
@@ -5,11 +5,35 @@ namespace Colecoes
 {
     class List
     {
-        static void Main(string[] args){
-            
+        static void Main(string[] args)
+        {
+
 
             Stack<string> pilhaLivros = new Stack<string>();
-            
+            Dictionary<string, string> ddds = new Dictionary<string, string>();
+
+            ddds.Add("SSA", "77");
+            ddds.Add("FSA", "75");
+            ddds.Add("outro estado", "81");
+
+            string valorProcurado = "FSA";
+
+            System.Console.WriteLine("Valor Original: ");
+            System.Console.WriteLine(ddds[valorProcurado]);
+
+            ddds[valorProcurado] = "75 - TESTE ATUALIZAÇÃO";
+            System.Console.WriteLine("Valor Atualizado: ");
+            System.Console.WriteLine(ddds[valorProcurado]);
+
+
+
+            // foreach (KeyValuePair<string,string> item in ddds)
+            // {
+            //     System.Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+            // }
+
+
+
             // pilhaLivros.Push(".NET");
             // pilhaLivros.Push("DDD");
             // pilhaLivros.Push("Código Limpo");
@@ -26,7 +50,7 @@ namespace Colecoes
             // fila.Enqueue("Dudu");
             // fila.Enqueue("André");
 
-            
+
 
             // System.Console.WriteLine($"Pessoas na fila: {fila.Count}");
             // while (fila.Count > 0){
@@ -44,9 +68,9 @@ namespace Colecoes
             // estados.Add("MG");
 
             // System.Console.WriteLine($"A quantidade de elementos na lista: {estados.Count}");
-            
-           
-            
+
+
+
             // // Adicionando outra coleção dentro da lista
             // // estados.AddRange(estadosArray);
 
@@ -64,4 +88,3 @@ namespace Colecoes
         }
     }
 }
-    
