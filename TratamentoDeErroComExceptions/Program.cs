1a7ce67e63a478fb481cc741a43b299a8736dda5
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-27 16:41:12 -0300

    usando try/catch

--- TratamentoDeErroComExceptions/Program.cs
+++ TratamentoDeErroComExceptions/Program.cs
@@ -1,12 +1,30 @@
-﻿// static int Dividir(int x, int y)
-// {
-//     return (x / y);
-// }
+﻿
 
-// int a = int.Parse(Console.ReadLine());
-// int b = int.Parse(Console.ReadLine());
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
 
-// int divisao = Dividir(a, b);
+try
+{
+    int divisao = Dividir(a, b);
 
-// System.Console.WriteLine(divisao);
+    System.Console.WriteLine(divisao);
+}
+catch (DivideByZeroException Exception)
+{
+    System.Console.WriteLine(Exception.Message);
+}
+
+catch (System.Exception Exception)
+{
+    System.Console.WriteLine(Exception.Message);
+
+}
+
+
+
+
+static int Dividir(int x, int y)
+{
+    return (x / y);
+}
 
