2ae9c551aed46a484162fa803b63a7c900790758
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-27 13:51:03 -0300

    aprendendo sobre exceptions

---
+++ TratamentoDeErroComExceptions/Program.cs
@@ -0,0 +1,11 @@
+﻿// static int Dividir(int x, int y)
+// {
+//     return (x / y);
+// }
+
+// int a = int.Parse(Console.ReadLine());
+// int b = int.Parse(Console.ReadLine());
+
+// int divisao = Dividir(a, b);
+
+// System.Console.WriteLine(divisao);
