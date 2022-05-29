3cce2571a012e64599e8ab53b4a20997f2dfbd66
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-29 03:30:53 -0300

    Utilizando StringWritter

---
+++ StringWritter/Program.cs
@@ -0,0 +1,32 @@
+﻿string textReaderText =
+                    "Perguntas de um Operário Letrado" +
+                    "Quem construiu Tebas, a das sete portas?" +
+                    "Nos livros vem o nome dos reis," +
+                    "Mas foram os reis que transportaram as pedras?\n\n" +
+
+                    "Babilónia, tantas vezes destruída," +
+                    "Quem outras tantas a reconstruiu? Em que casas" +
+                    "Da Lima Dourada moravam seus obreiros?" +
+
+                    "No dia em que ficou pronta a Muralha da China para onde" +
+                    "Foram os seus pedreiros? A grande Roma" +
+                    "Está cheia de arcos de triunfo. Quem os ergueu? Sobre quem\n\n";
+
+
+string linha, paragrafo = null;
+
+var sr = new StringReader(textReaderText);
+
+while (true)
+{
+    linha = sr.ReadLine();
+    if (linha != null)
+        paragrafo += linha + " ";
+    else
+    {
+        paragrafo += '\n';
+        break;
+    }
+
+}
+
