54b19048ed532ba632508a53c0ed15048cf6cfbf
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-29 03:20:05 -0300

    Manipulando buffer

--- Streams/Program.cs
+++ Streams/Program.cs
@@ -6,5 +6,21 @@ sb.AppendLine("e a segunda linha");
 sb.AppendLine("e o fim");
 
 using var sr = new StringReader(sb.ToString());
+var buffer = new char[10];
+var tamanho = 0;
 
-var texto = sr.ReadToEnd();
+
+
+do {
+    Console.WriteLine(sr.ReadLine());
+}while(sr.Peek() >= 0);
+
+// do{ 
+//     buffer = new char[10];
+//     tamanho = sr.Read(buffer);
+//     Console.WriteLine($"{string.Join("", buffer)}");
+
+// } while(tamanho >= buffer.Length);
+
+Console.WriteLine("Digite [enter] para finalizar");
+Console.ReadKey();
