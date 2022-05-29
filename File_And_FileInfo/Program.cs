705293609809614070ead69f549f3282cb180970
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-28 23:34:49 -0300

    criando função para Criar Arquivo

--- File_And_FileInfo/Program.cs
+++ File_And_FileInfo/Program.cs
@@ -2,12 +2,18 @@
 
 var path = Path.Combine(Environment.CurrentDirectory, "teste.txt");
 
-using var sw = File.CreateText(path);
 
-// Escrevendo em memória no arquivo.
-sw.WriteLine("Esta é a linha 1 do arquivo");
-sw.WriteLine("Esta é a linha 2 do arquivo");
-sw.WriteLine("Esta é a linha 3 do arquivo");
+static void CriarArquivo(string path)
+{
+    using var sw = File.CreateText(path);
 
-// Dando o flush para descarregar as linhas escritas da memória para o arquivo.
-sw.Flush();
+    // Escrevendo em memória no arquivo.
+    sw.WriteLine("Esta é a linha 1 do arquivo");
+    sw.WriteLine("Esta é a linha 2 do arquivo");
+    sw.WriteLine("Esta é a linha 3 do arquivo");
+
+    // Dando o flush para descarregar as linhas escritas da memória para o arquivo.
+    sw.Flush();
+}
+
+CriarArquivo(path);
