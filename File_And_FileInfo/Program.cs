7ed841cec7e435b290c9fe774ccaf8d0e9710446
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-28 23:29:50 -0300

    Aprendendo a mexer com arquivos

---
+++ File_And_FileInfo/Program.cs
@@ -0,0 +1,13 @@
+﻿// Criando arquivos.
+
+var path = Path.Combine(Environment.CurrentDirectory, "teste.txt");
+
+var sw = File.CreateText(path);
+
+// Escrevendo em memória no arquivo.
+sw.WriteLine("Esta é a linha 1 do arquivo");
+sw.WriteLine("Esta é a linha 2 do arquivo");
+sw.WriteLine("Esta é a linha 3 do arquivo");
+
+// Dando o flush para descarregar as linhas escritas da memória para o arquivo.
+sw.Flush();
