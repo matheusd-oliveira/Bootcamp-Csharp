0e45f10b3445c2171e62ed940dcb14317a9b49a7
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-29 02:39:00 -0300

    Usando FileSystemWatcher

---
+++ FileSystemWatcher/Program.cs
@@ -0,0 +1,27 @@
+﻿var path = @"C:\Users\mathe\Desktop\DIO\Directory_And_DirectoryInfo\globo";
+
+using var fsw = new FileSystemWatcher(path);
+fsw.Created += OnCreated;
+fsw.Renamed += OnRenamed;
+fsw.Deleted += OnDeleted;
+
+fsw.EnableRaisingEvents = true;
+fsw.IncludeSubdirectories = true;
+
+void OnCreated(object sender, FileSystemEventArgs e)
+{
+    throw new NotImplementedException();
+}
+
+
+void OnRenamed(object sender, RenamedEventArgs e)
+{
+    throw new NotImplementedException();
+}
+
+
+
+void OnDeleted(object sender, FileSystemEventArgs e)
+{
+    throw new NotImplementedException();
+}
