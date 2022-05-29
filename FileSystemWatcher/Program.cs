0e0cc662c851a51a74f894346be8417b912c24df
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-29 03:11:30 -0300

    Streams e Buffers

--- FileSystemWatcher/Program.cs
+++ FileSystemWatcher/Program.cs
@@ -8,14 +8,22 @@ fsw.Deleted += OnDeleted;
 fsw.EnableRaisingEvents = true;
 fsw.IncludeSubdirectories = true;
 
+Console.WriteLine("Monitorando eventos na pasta globo...");
+await Task.Delay(2000);
+
+Console.WriteLine("Pressione [ENTER] para finalizar...");
+Console.ReadLine();
+
 void OnCreated(object sender, FileSystemEventArgs e)
 {
+    Console.WriteLine($"Foi criado o arquivo {e.Name}");
     throw new NotImplementedException();
 }
 
 
 void OnRenamed(object sender, RenamedEventArgs e)
 {
+    Console.WriteLine($"O arquivo {e.OldName} renomeado para o arquivo {e.Name}");
     throw new NotImplementedException();
 }
 
@@ -23,5 +31,6 @@ void OnRenamed(object sender, RenamedEventArgs e)
 
 void OnDeleted(object sender, FileSystemEventArgs e)
 {
+    Console.WriteLine($"Foi deletado o arquivo {e.Name}");
     throw new NotImplementedException();
 }
---
+++ Streams/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+var sb = new StringBuilder();
+sb.AppendLine("Caracteres na primeira linha para ler");
+sb.AppendLine("e a segunda linha");
+sb.AppendLine("e o fim");
+
+using var sr = new StringReader(sb.ToString());
+
+var texto = sr.ReadToEnd();
