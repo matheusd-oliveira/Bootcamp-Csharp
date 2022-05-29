ea5f66f0a390e93bca15dd2a715bb9ac0528dd1a
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-29 03:43:33 -0300

    update

--- StringWritter/Program.cs
+++ StringWritter/Program.cs
@@ -4,17 +4,17 @@
                     "Nos livros vem o nome dos reis," +
                     "Mas foram os reis que transportaram as pedras?\n\n" +
 
-                    "Babilónia, tantas vezes destruída," +
+                    "Babilónia, tantas vezes destruída." +
                     "Quem outras tantas a reconstruiu? Em que casas" +
                     "Da Lima Dourada moravam seus obreiros?" +
 
                     "No dia em que ficou pronta a Muralha da China para onde" +
-                    "Foram os seus pedreiros? A grande Roma" +
-                    "Está cheia de arcos de triunfo. Quem os ergueu? Sobre quem\n\n";
+                    "Foram os seus pedreiros? A grande Roma." +
+                    "Está cheia de arcos de triunfo. Quem os ergueu? Sobre quem.\n\n";
 
 
+Console.WriteLine(textReaderText);
 string linha, paragrafo = null;
-
 var sr = new StringReader(textReaderText);
 
 while (true)
@@ -27,6 +27,34 @@ while (true)
         paragrafo += '\n';
         break;
     }
-
 }
 
+Console.WriteLine($"Texto modificado: {paragrafo}");
+
+int caractereLido;
+char caracterConvertido;
+
+var sw = new StringWriter();
+sr = new StringReader(paragrafo);
+while (true)
+{
+    caractereLido = sr.Read();
+    if(caractereLido == -1) 
+        break;
+    
+    caracterConvertido = Convert.ToChar(caractereLido);
+
+    if (caracterConvertido == '.')
+    {
+        sw.Write("\n\n");
+        sr.Read();
+        sr.Read();
+ 
+    }
+    else
+    {
+        sw.Write(caracterConvertido);
+    }
+}
+
+Console.WriteLine($"Texto armazenado no StringWriter: {sw.ToString()}");
