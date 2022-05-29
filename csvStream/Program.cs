ec57f119379f47d263927153ae10bd5183c97ec5
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-29 17:28:30 -0300

    Lendo arquivos csv

---
+++ csvStream/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+var path = Path.Combine(
+    Environment.CurrentDirectory,
+    "Entrada",
+    "usuarios-exportacao.csv");
+
+using var sr = new StreamReader(path);
+var cabecalho = sr.ReadLine()?.Split(',');
+
+while (true)
+{
+    var registro = sr.ReadLine()?.Split(',');
+    if (registro == null)
+        break;
+    
+    for (int i = 0; i < registro.Length; i++)
+    {
+        WriteLine($"{cabecalho?[i]}: {registro[i]}");
+    }
+
+    WriteLine("---------------");
+    WriteLine("Pressione [ENTER] para finalizar...");
+    ReadLine();
+}
