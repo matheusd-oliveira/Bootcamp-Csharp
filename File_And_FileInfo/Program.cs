6ad69a021943ce1939f49a43d3a703219f525566
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-05-28 23:46:26 -0300

    Colocando o tratamento em uma função LimparNome

--- File_And_FileInfo/Program.cs
+++ File_And_FileInfo/Program.cs
@@ -3,6 +3,10 @@
 // Criando arquivos.
 Write("Digite o nome do arquivo: ");
 var nome = ReadLine();
+
+// Tratando erros com GetInvalidFileNameChars
+nome = LimparNome(nome);
+
 var path = Path.Combine(Environment.CurrentDirectory, $"{nome}.txt");
 
 
@@ -12,6 +16,14 @@ WriteLine();
 WriteLine("Digite enter para finalizar...");
 ReadLine();
 
+static string LimparNome(string nome)
+{
+    foreach (var @char in Path.GetInvalidFileNameChars())
+    {
+        nome = nome.Replace(@char, '-');
+    }
+    return nome;
+}
 static void CriarArquivo(string path)
 {
     try
